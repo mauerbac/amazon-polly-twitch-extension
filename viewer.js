158142c557e1b1d714497a79993cc09c287fe84b
Matt Auerbach <tennisfan0526@gmail.com>
2019-01-03 10:44:43 -0500

    fixes

--- viewer.js
+++ viewer.js
@@ -1,37 +1,36 @@
-var token = "";
-var tuid = "";
-var ebs = "";
+let token = '';
+let tuid = '';
 
-var twitch = window.Twitch.ext;
+let twitch = window.Twitch.ext;
 
-twitch.onContext(function(context) {
-    twitch.rig.log(context);
+twitch.onContext(function (context) {
+  twitch.rig.log(context);
 });
 
-twitch.onAuthorized(function(auth) {
-    // save our credentials
-    token = auth.token;
-    tuid = auth.userId;
+twitch.onAuthorized(function (auth) {
+  // save our credentials
+  token = auth.token;
+  tuid = auth.userId;
 
-    // enable the button
-    $('#play_sound').removeAttr('disabled');
+  // enable the button
+  $('#play_sound').removeAttr('disabled');
 });
 
 $(function() {
-    $('#play_sound').click(function() {
-        if(!token) { return twitch.rig.log('Not authorized'); }
-        twitch.rig.log('Requesting an audio file');
+  $('#play_sound').click(function () {
+    if(!token) { return twitch.rig.log('Not authorized'); }
+      twitch.rig.log('Requesting an audio file');
 
-        // current time in user timezone
-        var currentTime = new Date().toLocaleTimeString();
-        // form message
-        var textToPlay = 'Hello! The current time is ' + currentTime
+    // current time in user timezone
+    const currentTime = new Date().toLocaleTimeString();
+    // form message
+    const textToPlay = 'Hello! The current time is ' + currentTime;
 
-        player.src = '//127.0.0.1:8000/read?voiceId=' +
-                        encodeURIComponent('Joanna') +
+    player.src = '//127.0.0.1:8000/read?voiceId=' +
+      encodeURIComponent('Joanna') +
                         '&text=' + encodeURIComponent(textToPlay) +
                         '&outputFormat=' + 'mp3';
-        player.play();
-        event.preventDefault();
-    });
+    player.play();
+    event.preventDefault();
+  });
 });
